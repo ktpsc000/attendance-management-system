19497bd0c3d52073c38a871e2ffd1657a035894a
Ryo <ktpsc000@gmail.com>
2026-05-21 18:00:50 +0900

    add：migrations、Models

---
+++ src/database/migrations/2026_05_21_163412_create_correction_requests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCorrectionRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('correction_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('attendance_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->dateTime('request_clock_in_at')->nullable();
+            $table->dateTime('request_clock_out_at')->nullable();
+            $table->text('remarks')->nullable();
+            $table->tinyInteger('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('correction_requests');
+    }
+}
---
+++ src/database/migrations/2026_05_21_163414_create_break_correction_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBreakCorrectionRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('break_correction_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('correction_attendance_id')->constrained()->cascadeOnDelete();
+            $table->datetime('break_start_at')->nullable();
+            $table->datetime('break_end_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('break_correction_requests');
+    }
+}
